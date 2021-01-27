199b6be92a9603879aba1a36beeaefa15eb5de64
JebediahKermanBadS <pfister.marc-it19@it.dhbw-ravensburg.de>
2021-01-27 10:29:36 +0100

    Reading the /dev/mem file should work.
    Timer and Interrupts are still a TODO

--- src/main.s
+++ src/main.s
@@ -6,13 +6,14 @@
 @@@		- Grajczak, Benjamin
 @@@		- Pfister, Marc
 @@@ target:	 Raspberry Pi Zero
-@@@	date:	 2020/01.22
+@@@	date:	 2020/01/26
 @@@	verison: 1.0.0
 @@@ --------------------------------------------------------------------------
 
+
 @@@ Constants defining the flags for opening the gpiomem file
 @@@ Defined in /usr/include/asm-generic/fcntl.h
-	.equ O_RDWR, 	2  				@ Read and Write the file.
+	.equ O_RDWR, 	02				@ Read and Write the file.
 	.equ O_DSYNC, 	010000
 	.equ O_SYNC, 	04000000|O_DSYNC
 	.equ O_FLAGS, 	O_RDWR|O_SYNC
@@ -22,10 +23,14 @@
 	.equ PROT_RW, 		0x01|0x02	@ Can read(0x01) and write(0x02) the memory
 	.equ MAP_SHARED,	0x01		@ Share the memory with oder processes
 
-	.equ PERIPH,		0x20000000
-	.equ GPIO_OFFSET,	0x200000
+	.equ PERIPH,		 0x20000000
+	.equ GPIO_OFFSET,	 0x200000
+	.equ TIMERIR_OFFSET, 0xB000
 
 	.equ PAGE_SIZE, 4096
+
+
+
 
 @@@ Define the offset for the GPIO Registers
 @@@ --------------------------------------------------------------------------
@@ -38,56 +43,101 @@
 	.equ OUTPUT,	0b001
 	.equ INPUT,		0b000
 
-@@@ Define constants for the connected pins
-@@@ --------------------------------------------------------------------------
+
+@@@ Pins for the 7-Segment Display -------------------------------
+	.equ pin_SER,		2
+	.equ pin_SRCLK,		3
+	.equ pin_nSRCLR,	4
+	.equ pin_RCLK,		5
+	.equ pin_SEG_A,		6
+	.equ pin_SEG_B,		7
+
+@@@ Pins of the Buttons ------------------------------------------
 	.equ pin_nBTN1,		8
 	.equ pin_nBTN2,		9
 	.equ pin_nBTN3,		10
-@@@ --------------------------------------------------------------------------
+
+@@@ Pins of the Outlet -------------------------------------------
+	.equ pin_nRSTOut,	11
+	.equ pin_StepOut,	12
+
+@@@ Pins of the Color-Wheel --------------------------------------
 	.equ pin_StepCW,	13
 	.equ pin_DirCW,		16
 	.equ pin_nRSTCW,	17
-@@@ --------------------------------------------------------------------------
+
+@@@ Pin of the Color-LEDs ----------------------------------------
+@@@	.equ ledSig, 		18
+
+@@@ Pin of the Feeder --------------------------------------------
 	.equ pin_feeder,	19
-@@@ --------------------------------------------------------------------------
+
+@@@ Pins of the Hallsensor ---------------------------------------
+	.equ pin_nHallCW,		20
+	.equ pin_nHallOutlet,	21
+
+@@@ Pins for the color recognition -------------------------------
 	.equ pin_colorBit1,	22
 	.equ pin_colorBit2,	23
 	.equ pin_colorBit3,	24
 
+@@@ Pins for the objectsensor in the outlet ----------------------
+	.equ pin_objCW,		25
+	.equ pin_dirOut,	26
 
-@@@ Rename some registers
-@@@ --------------------------------------------------------------------------
+@@@ Pin to let the co-processor sleep ----------------------------
+	.equ pin_nSLP,		27
+
+@@@ --------------------------------------------------------------
+@@@ Colors from the Co-Prozessor ---------------------------------
+	.equ C_RED, 	0b001
+	.equ C_GREEN,	0b010
+	.equ C_BLUE,	0b011
+	.equ C_BRONW,	0b100
+	.equ C_ORANGE,	0b101
+	.equ C_YELLOW,	0b110
+	.equ C_UNKNOWN, 0b000
+
+@@@ --------------------------------------------------------------
+@@@ Renamimg registers -------------------------------------------
+	rTIMER	.req r9
 	rGPIO	.req r10
 
 
-@@@ Start Data Section
-@@@ --------------------------------------------------------------------------
+@@@ --------------------------------------------------------------
+@@@ Start Data Section -------------------------------------------
 .data
 .align 4
 
-msg_init: .asciz "M&M Sorting Machine started!\nThis is a program from the following group:\n\t- Demiroez, Dilara\n\t- Gonther, Levin\n\t- Graiczak, Benjamin\n\t- Pfister, Marc\n"
+msg_init: .asciz "M&M Sorting Machine started!\nThis is a program from the following group:\n\t- Demiroez, Dilara\n\t- Gonther, Levin\n\t- Grajczak, Benjamin\n\t- Pfister, Marc\n"
 
-msg_fd: .asciz "File descriptor is: %d\n"
-msg_gpio_mem: .asciz "Gpio memory is: %p\n"
+file_mem:		.asciz "/dev/mem"
+file_gpiomem: 	.asciz "/dev/gpiomem"
 
-msg_closeFile_succes: .asciz "Succesfully closed the file /dev/gpiomem\n"
-msg_closeFile_failure: .asciz "Failure closing the file /dev/gpiomem\n"
+file_open_failure: .asciz "Failure! Cant open the file %s. Try execute wiht sudo\n"
+file_close_failure: .asciz "Failure! Not possible to close the file %s correctly.\n"
+gpio_munmap_failure: .asciz "Failure unmapping the gpio memory!\n"
 
-msg_munmap_succes: .asciz "Gpio memory succesfully unmapped!\n"
-msg_munmap_failure: .asciz "Failure unmapping the gpio memory!\n"
-
-gpio_file: .asciz "/dev/gpiomem"
-gpio_openMode: .word O_FLAGS
+msg_gpio_mem: 	.asciz "Gpio memory is: %p\n"
+msg_timer_mem: .asciz "Timer memory is: %p\n"
 
 msg_print_int: .asciz "%d\n"
 msg_print_hex: .asciz "%x\n"
 
-@@@ --------------------------------------------------------------------------
-@@@ Start Text Section
-@@@ --------------------------------------------------------------------------
+.align 4
+gpio_fsel0_clear: 	@0011 1111 1111 1111 1111 1111 1100 0000
+			.word 	0x3FFFFFC0
+.align 4
+gpio_fsel0_output: 	@0000 1001 0010 0100 1001 0000 0000 0000
+			.word	0x09249000
+
+@@@ --------------------------------------------------------------
+@@@ Start Text Section -------------------------------------------
 .text
 
-gpio_addr: .word PERIPH+GPIO_OFFSET
+openMode:	.word O_FLAGS
+gpio: 		.word PERIPH + GPIO_OFFSET
+timerIR: 	.word PERIPH + TIMERIR_OFFSET
 
 .extern printf
 .extern open
@@ -95,6 +145,8 @@ gpio_addr: .word PERIPH+GPIO_OFFSET
 .extern mmap
 .extern munmap
 .extern sleep
+
+.extern init_gpiomem
 
 .global main
 main:
@@ -106,58 +158,136 @@ main:
 
 init_gpio_mem:
 	@ Opening the file /dev/gpiomem with read/write access
-	ldr r0, =gpio_file
-	ldr r1, =gpio_openMode
-	ldr r1, [r1]
+	ldr r0, =file_gpiomem
+	ldr r1, openMode
 	bl open
 
 	@ Store the file descriptor (r0) on the top of the stack
 	sub sp, sp, #8
 	str r0, [sp]
 
-	@ Display the file descriptor
-	ldr r1, [sp]
-	ldr r0, =msg_fd
-	bl printf
+	@ Check if there are any errors
+	cmp r0, #-1
+	ldreq r0, =file_open_failure
+	ldreq r1, =file_gpiomem
+	bleq printf
+	beq main_end
 
 	@ Map the gpio registers
-	ldr r0, gpio_addr
+	ldr r0, gpio
 	str r0, [sp, #4]
-	mov r0, #0			@ No prefer where to allocate the memory
+	mov r0, #0				@ No prefer where to allocate the memory
 	mov r1, #PAGE_SIZE
 	mov r2, #PROT_RW
 	mov r3, #MAP_SHARED
 	bl mmap
-
-	@ Store the virtual address
+	@ Store and print the virtual address
 	mov rGPIO, r0
+	ldr r0, =msg_gpio_mem
+	mov r1, rGPIO
+	bl printf
 
 	@ Closing the gpio file and restore stack pointer
 	ldr r0, [sp], #8
 	bl close
 
-	@ Print succesfull closing
+	@ Print an error message if its not possible to close the file
 	cmp r0, #0
-	ldreq r0, =msg_closeFile_succes
-	ldrne r0, =msg_closeFile_failure
+	ldrne r1, =file_gpiomem
+	ldrne r0, =file_close_failure
+	blne printf
+
+@@@ --------------------------------------------------------------
+@@@ Open the file /dev/mem for the timer and interrupts ----------
+	ldr r0, =file_mem
+	ldr r1, openMode
+	bl open
+
+	@ Store the file descriptor (r0) on the top of the stack
+	sub sp, sp, #8
+	str r0, [sp]
+
+	@ Print an error message if its not possible to open the file
+	cmp r0, #-1
+	ldreq r0, =file_open_failure
+	ldreq r1, =file_mem
+	bleq printf
+	beq main_end_unmapgpio
+
+	@ Map the timer reigsters
+	ldr r0, timerIR
+	str r0, [sp, #4]
+	mov r0, #0				@ No prefer where to allocate the memory
+	mov r1, #PAGE_SIZE
+	mov r2, #PROT_RW
+	mov r3, #MAP_SHARED
+	bl mmap
+
+	@ Store and print the virtual address
+	mov rTIMER, r0
+	ldr r0, =msg_timer_mem
+	mov r1, rTIMER
 	bl printf
 
-	@ Printing the virtual address
-	ldr r0, =msg_gpio_mem
-	mov r1, rGPIO
-	bl printf
+	@ Closing the mem file and restore stack pointer
+	ldr r0, [sp], #8
+	bl close
+
+	@ Print an error message if its not possible to close the file
+	cmp r0, #0
+	ldrne r0, =file_close_failure
+	ldrne r1, =file_mem
+	blne printf
 
 init_hardware:
-@@@ Set the first alternate function select register -------------------------
+
+	mov r1, #1
+	str r1, [rTIMER, #0x18]
+
+	ldr r0, =msg_print_hex
+	ldr r1, [rTIMER,#0x408]
+	mov r4, r1
+	bl printf
+
+	orr r4, r4, #0b101 << 5
+	str r4, [rTIMER,#0x408]
+
+	ldr r0, =msg_print_hex
+	mov r1, r4
+	bl printf
+
+	ldr r0, =msg_print_hex
+	ldr r1, [rTIMER,#0x408]
+	bl printf
+
+	ldr r1, [rTIMER, #0x218]
+	orr r1, r1, #0x01
+	@str r1, [rTIMER, #0x218]
+
+	ldr r1, [rTIMER, #0x218]
+	ldr r0, =msg_print_hex
+	bl printf
+
+@@@ Set the first alternate function select register -------------
 	ldr r4, [rGPIO]
 
-	@ Define button1 and 2 as input
-	bic r4, r4, #0b111111 << 24
+	@mov r1, r4
+	@ldr r0, =msg_print_hex
+	@@bl printf
+
+	@ Define pins 2-9 as input
+	ldr r5, =gpio_fsel0_clear
+	ldr r5, [r5]
+	bic r4, r4, r5
+
+	@ Define pins 2-7 as output
+	ldr r5, =gpio_fsel0_output
+	ldr r5, [r5]
+	orr r4, r4, r5
 
 	str r4, [rGPIO]
-@@@ --------------------------------------------------------------------------
 
-@@@ Set the second alternate function select register ------------------------
+@@@ Set the second alternate function select register ------------
 	ldr r4, [rGPIO, #GPFSEL1]
 
 	@ Define button3 as input
@@ -198,10 +328,13 @@ main_loop:
 
 	@ Setting the feeder to off
 	ldr r1, [rGPIO, #GPCLR0]
-	orr r1, r1, r4, LSL #pin_feeder
+	orr r1, r1, r4, LSL #19
 	str r1, [rGPIO, #GPCLR0]
 
-main_end:
+main_end_unmap:
+
+
+main_end_unmapgpio:
 
 	@ Unmap the memory
 	mov r0, rGPIO
@@ -210,15 +343,20 @@ main_end:
 
 	@ Display munmap success
 	cmp r0, #-1
-	ldrne r0, =msg_munmap_succes
-	ldreq r0, =msg_munmap_failure
-	bl printf
+	ldreq r0, =gpio_munmap_failure
+	bleq printf
 
+main_end:
 	mov sp, fp
 	pop {fp, lr}
 	bx lr
 
+irq:
+	push {r0, r1, lr}
+	ldr r0, =msg_init
+	bl printf
 
+	pop {r0, r1, lr}
 
 
 
