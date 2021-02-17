4b123c49f298d3ad8337c73db406416e3fe4ef04
JebediahKermanBadS <pfister.marc-it19@it.dhbw-ravensburg.de>
2021-02-17 23:05:56 +0100

    Button to start stop and improve calibration

--- src/main.s
+++ src/main.s
@@ -31,9 +31,19 @@
 	.equ pin_dirOut,	26
 
 @@@ Define the offset for the GPIO Registers ------------------------------------------------
+	.equ GPFSET0, 	0x00
+	.equ GPFSET1, 	0x04
 	.equ GPSET0,	0x1C
 	.equ GPCLR0,	0x28
 	.equ GPLVL0,	0x34
+
+@@@ For the buttons -------------------------------------------------------------------------
+	.equ GPEDS0,	0x40
+	.equ GPREN0,	0x4C
+	.equ GPFEN0,	0x58
+	.equ GPHEN0,	0x64
+	.equ GPLEN0,	0x70
+
 
 .data
 .align 4
@@ -45,6 +55,11 @@ msg_timer_mem: 	.asciz "Timer memory is: %p\n"
 
 msg_print_int: 	.asciz "%d\n"
 msg_print_hex: 	.asciz "%x\n"
+
+msg_calibration_finished: .asciz "The calibration of the outlet and the color wheel is finished.\n"
+
+.align 4
+is_running:	.word 0
 
 .align 4
 color_array: .word 0
@@ -59,7 +74,9 @@ outlet_position: .word 0
 
 .text
 
-timer_pre_divider: .word 399
+timer_pre_divider: 	.word 399
+
+addr_is_running: 	.word is_running
 
 .extern printf
 .extern sleep
@@ -132,10 +149,14 @@ main:
 	bl leds_Init
 	bl outlet_init
 	bl timer_init
+	bl interrupts_disable
+	bl buttons_init
 
 	mov r0, #1
 	bl colow_wheel_set_enable
-
+	mov r0, #1
+	bl timer_set_enable
+	bl cop_wakeup
 	calibrate_loop:
 		ldr r0, [rTIMER, #0x410]
 		cmp r0, #0
@@ -145,10 +166,204 @@ main:
 		str r0, [rTIMER, #0x40C]
 		bl color_wheel_calibrate
 
-		b calibrate_loop
+		cmp r0, #0
+		bgt calibrate_loop
 
+	bl colow_wheel_set_enable
+	mov r0, #0
+	bl timer_set_enable
+	bl cop_sleep
+
+	ldr r0, =msg_calibration_finished
+	bl printf
+
+	main_loop:
+		ldr r0, [rGPIO, #GPEDS0]
+		ands r0, #1 << pin_nBTN1
+		beq main_loop
+
+		str r0, [rGPIO, #GPEDS0]
+
+		ldr r0, addr_is_running
+		ldr r0, [r0]
+		cmp r0, #0
+		str r0, [sp, #-4]!
+		bleq machine_start
+		ldr r0, [sp], #4
+		cmp r0, #1
+		bleq machine_stop
+
+		b main_loop
 
 	b main_munmap_pgpio
+
+
+main_munmap_pgpio:
+	mov r0, rTIMER
+	bl unmap_memory
+
+	mov r0, rGPIO
+	bl unmap_memory
+
+main_end:
+	bl leds_DeInit
+
+	mov sp, fp
+	pop {fp, lr}
+	bx lr
+
+machine_start:
+	push {lr}
+
+	bl feeder_on
+	bl cop_wakeup
+	mov r0, #1
+	bl colow_wheel_set_enable
+	mov r0, #1
+	bl timer_set_enable
+
+	mov r1, #1
+	ldr r0, addr_is_running
+	str r1, [r0]
+
+	pop {lr}
+	bx lr
+
+machine_stop:
+	push {lr}
+	bl feeder_off
+	bl cop_sleep
+	mov r0, #0
+	bl colow_wheel_set_enable
+	mov r0, #0
+	bl timer_set_enable
+
+	mov r1, #0
+	ldr r0, addr_is_running
+	str r1, [r0]
+
+	pop {lr}
+	bx lr
+
+swap_led_state:
+	ldr r0, [rGPIO, #GPLVL0]
+	ands r0, #1 << 19
+	mov r0, #1 << 19
+	streq r0, [rGPIO, #GPSET0]
+	strne r0, [rGPIO, #GPCLR0]
+	bx lr
+
+@@@ -----------------------------------------------------------------------------------------
+@@@ Enable the buttons
+@@@ Inputs: None
+@@@ Return: None
+buttons_init:
+	ldr r0, [rGPIO, #GPFSET0]
+
+	@ Set the button at pin 8 as input
+	bic r0, #0b111 << 24
+
+	@ Set the button at pin 9 as input
+	bic r0, #0b111 << 27
+
+	str r0, [rGPIO, #GPFSET0]
+
+	@ Set the button at pin 10 as input
+	ldr r0, [rGPIO, #GPFSET1]
+	bic r0, #0b111
+	str r0, [rGPIO, #GPFSET1]
+
+	@ Enable RISING EDGE detect for all buttons
+	ldr r0, [rGPIO, #GPREN0]
+	orr r0, #0b111 << pin_nBTN1
+	str r0, [rGPIO, #GPREN0]
+
+	@ Disable FALLING EDGE detect for all buttons
+	ldr r0, [rGPIO, #GPFEN0]
+	bic r0, #0b111 << pin_nBTN1
+	str r0, [rGPIO, #GPFEN0]
+
+	@ Disable HIGH detect for all buttons
+	ldr r0, [rGPIO, #GPHEN0]
+	bic r0, #0b111 << pin_nBTN1
+	str r0, [rGPIO, #GPHEN0]
+
+	@ Disable LOW detect for all buttons
+	ldr r0, [rGPIO, #GPLEN0]
+	bic r0, #0b111 << pin_nBTN1
+	str r0, [rGPIO, #GPLEN0]
+	bx lr
+
+@@@ -----------------------------------------------------------------------------------------
+@@@ Disable interrupts
+@@@ Inputs: None
+@@@ Return: None
+interrupts_disable:
+	@ Disable timer interrupt
+	mov r0, #1
+	str r0, [rTIMER, #0x224]
+
+	@ Disable gpio interrupts
+	mov r0, #0b1111 << 17
+	str r0, [rTIMER, #0x220]
+
+	bx lr
+
+@@@ -----------------------------------------------------------------------------------------
+@@@ Initialize the timer
+@@@ Inputs: None
+@@@ Return: None
+timer_init:
+
+	@ Set the control register of the timer
+	ldr r0, [rTIMER, #0x408]
+	bic r0, #1 << 7 	@ Disable the timer
+	bic r0, #1 << 5 	@ Disable the timer interrupt
+	bic r0, #11 << 2	@ Disable the prescale
+	bic r0, #1 << 1		@ Set to 16-bit timer
+	str r0, [rTIMER, #0x408]
+
+	@ Clear the interrupt pending bit
+	mov r0, #1
+	str r0, [rTIMER, #0x40C]
+
+	@ Set the pre-devider of the timer, fTimer = 1MHz
+	ldr r0, timer_pre_divider
+	str r0, [rTIMER, #0x41C]
+
+	@ Set the load value of the timer to 1000 ; fTimer = 1kHz
+	mov r0, #1000
+	str r0, [rTIMER, #0x400]
+
+	@ Enable the timer after all settings are made
+	ldr r0, [rTIMER, #0x408]
+	orr r0, #1 << 7
+	str r0, [rTIMER, #0x408]
+
+	bx lr
+
+@@@ -----------------------------------------------------------------------------------------
+@@@ Enable or disable the timer
+@@@ Inputs: r0 >= 1 -> enabled
+@@@			r0 == 0 -> disabled
+@@@ Return: None
+timer_set_enable:
+	cmp r0, #0
+
+	@ Set the control register of the timer
+	ldr r0, [rTIMER, #0x408]
+	biceq r0, #1 << 7 	@ Disable the timer
+	orrne r0, #1 << 7 	@ Enable the timer
+	str r0, [rTIMER, #0x408]
+
+	bx lr
+
+
+
+
+
+
+
 
 /*
 calibration:
@@ -197,144 +412,6 @@ main_loop:
 	no_rotation:
 		b main_loop
 */
-
-main_munmap_pgpio:
-	mov r0, rTIMER
-	bl unmap_memory
-
-	mov r0, rGPIO
-	bl unmap_memory
-
-main_end:
-	bl leds_DeInit
-
-	mov sp, fp
-	pop {fp, lr}
-	bx lr
-
-
-
-timer_init:
-	push {lr}
-
-	@ Disable the timer interrupt
-	mov r0, #1
-	str r0, [rTIMER, #0x224]
-
-	@ Set the control register of the timer
-	ldr r0, [rTIMER, #0x408]
-	bic r0, #1 << 7 	@ Disable the timer
-	bic r0, #1 << 5 	@ Disable the timer interrupt
-	bic r0, #11 << 2	@ Disable the prescale
-	bic r0, #1 << 1		@ Set to 16-bit timer
-	str r0, [rTIMER, #0x408]
-
-	@ Clear the interrupt pending bit
-	mov r0, #1
-	str r0, [rTIMER, #0x40C]
-
-	@ Set the pre-devider of the timer, fTimer = 1MHz
-	ldr r0, timer_pre_divider
-	str r0, [rTIMER, #0x41C]
-
-	@ Set the load value of the timer to 1000 ; fTimer = 1kHz
-	mov r0, #1000
-	str r0, [rTIMER, #0x400]
-
-	@ Enable the timer after all settings are made
-	ldr r0, [rTIMER, #0x408]
-	orr r0, #1 << 7
-	str r0, [rTIMER, #0x408]
-
-	pop {lr}
-	bx lr
-
-
-
-testing_components:
-	push {r4, lr}
-
-@@@ Testing the co-processor and set it to on
-	bl cop_wakeup
-
-@@@ Setting the feeder to on for 2 seconds
-	bl feeder_on
-
-	@ Sleep 2 seconds
-	mov r0, #2
-	bl sleep
-
-	bl feeder_off
-
-	@ Sleep 2 second
-	mov r0, #2
-	bl sleep
-
-@@@ Calibrate and rotate the color wheel 2 times
-	bl color_wheel_calibrate
-
-	@ Sleep 2 second
-	mov r0, #2
-	bl sleep
-
-	bl color_wheel_rotate90
-
-	@ Sleep 2 second
-	mov r0, #2
-	bl sleep
-
-	bl color_wheel_rotate90
-
-	@ Sleep 2 seconds
-	mov r0, #2
-	bl sleep
-
-@q@ Calibrate and rotate the outlet twice by +90° and once by -90°
-	bl outlet_calibrate
-
-	@ Sleep 2 seconds
-	mov r0, #2
-	bl sleep
-
-	bl outlet_rotate60_clockwise
-
-	@ Sleep 2 seconds
-	mov r0, #2
-	bl sleep
-
-	bl outlet_rotate60_clockwise
-
-	@ Sleep 2 seconds
-	mov r0, #2
-	bl sleep
-
-	bl outlet_rotate60_counterclockwise
-
-@@@ Show all colors in the order: Yellow, Orange, Brown, Blue, Green, Red
-	mov r4, #5
-test_loop:
-	mov r0, r4
-	bl leds_showColor
-
-	@ Sleep 2 seconds
-	mov r0, #1
-	bl sleep
-
-	subs r4, r4, #1
-	bpl test_loop
-
-	pop {r4, lr}
-	bx lr
-
-
-
-
-
-
-
-
-
-
 
 
 
